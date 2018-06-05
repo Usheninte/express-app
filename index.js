624e213c969417b2af25660b5394292590484422
Usheninte <jrandangana@gmail.com>
2018-06-05 14:11:24 +0100

    finished Essential Express Training course

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import favicon from 'serve-favicon';
+import path from 'path';
 import data from './data/data.json';
 
 const app = express();
@@ -7,27 +9,70 @@ const PORT = 3000;
 // this is for the pblic folder on path "/"
 app.use(express.static('public'));
 
+// this is the method to use JSON
+// app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// this is for proxies
+app.set('trust proxy', 'loopback');
+
 // this is for the images folder on path "images"
 app.use('/images', express.static('images'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.get('/', (req, res) => 
     // get data first
     res.json(data)
-)
-
-app.post('/newItem', (req, res) => 
-    res.send(`a post resquest with /newItem route on port ${PORT}`)
 );
 
-app.put('/item', (req, res) => 
-    res.send(`a put resquest with /item route on port ${PORT}`)
+// JSON data
+// { "hello": "JSON is cool" }
+// URLEndoded data
+// hello=URLEncoded+is+cool
+
+app.post('/newItem', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.get('/item/:id', (req, res, next) => {
+    // remember to place the : column on every params that is passed, such as the id above
+    // this the middleware that pulls the data
+    console.log(req.params.id);
+    let user = Number(req.params.id);
+    console.log(user);
+    console.log(data[user]);
+    // middleware that uses the req [REQUEST] object
+    console.log(`Request from: ${req.originalUrl}`);
+    console.log(`Request type: ${req.method}`);
+    // everything above is middleware
+    res.send(data[user]);
+    next();
+}, (req, res) => 
+    console.log('Did you get the right data?')
 );
 
-app.delete('/item', (req, res) => 
-    res.send(`a delete resquest with /item route on port ${PORT}`)
+app.route('/item')
+    .get((req, res) => {
+        throw new Error();
+        // res.download('images/rocket.jpg')
+        // res.redirect('http://www.linkedin.com')
+        // res.end()
+        // res.send(`a get request with /item route on port ${PORT}`)
+    })
+    .put((req, res) => 
+        res.send(`a put request with /item route on port ${PORT}`)
+    )
+    .delete((req, res) => 
+        res.send(`a delete request with /item route on port ${PORT}`)
 );
+
+// error handling function
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send(`Red Alert! Red Alert! :: ${err.stack}`)
+});
 
 app.listen(PORT, () => {
     console.log(`Your server is running on port ${PORT}`);
-    console.log(data);
 });
